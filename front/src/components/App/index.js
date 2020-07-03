36a0e8da5dd194b3ce39c9b46d942457970b7261
Emilie-Excalibur <emiliemaniglier@gmail.com>
2020-07-03 14:10:11 +0200

    :building_construction: Create footer
    Modification of the variable for the main light color

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 // == Local import
 import Header from 'src/components/Header';
+import Footer from 'src/components/Footer';
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
     <Header />
+    <Footer />
   </div>
 );
 
