3c579316c3ee61bf5465e4ee2430809b20e55f9c
Emilie-Excalibur <emiliemaniglier@gmail.com>
2020-07-07 11:12:22 +0200

    :lipstick: Modify Suggestions Styles

--- front/src/components/Suggestions/Suggestion.js
+++ front/src/components/Suggestions/Suggestion.js
@@ -6,14 +6,17 @@ import './suggestion.scss';
 const Suggestion = () => (
   <div className="suggestion-container">
     <div className="picture-activity">
-      <img src={climbing} alt="Faire de l'escalade" className="" />
+      <img src={climbing} alt="Faire de l'escalade" className="picture" />
     </div>
-    <p className="category">
-      Catégorie : Sport
-    </p>
-    <p className="name-activity">
-      Faire de l'escalade
-    </p>
+    <div className="suggestion">
+      <h3 className="">Categorie : </h3>
+      <p className="name-category">
+        Sport
+      </p>
+      <p className="name-activity">
+        Faire de l'escalade
+      </p>
+    </div>
   </div>
 );
 
