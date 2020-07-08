8a4ce2c8d308e1bbe66cc48fb65c2089262b8515
raspnotification <raspnotification@gmail.com>
2020-07-08 09:43:10 +0200

    :construction: Format the tableidea array to return a correct JSON object

--- back/src/Controller/ActivityController.php
+++ back/src/Controller/ActivityController.php
@@ -38,7 +38,7 @@ class ActivityController extends AbstractController
      */
     public function suggestion()
     {
-        $tokenService = $this->jwtDecodeService->tokenDecode("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1OTQxMjE3MzgsImV4cCI6MTU5NDEyNTMzOCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoicXVlbnRpbi5iYXJyYXVkQGdtYWlsLmNvbSJ9.P0XZASjRX_OInfgU7gSRLJrt7hMbkqQ1-hZtuOkDd2QDnnyYLM4HBfC33l21_9BsbZbZFHCZOQtPjqX_T6Tb00rmhXkJudaCZbVYkM3bVWfegilsmdRIZu6zAfDSuMRYFO4dCjuHF3zPcJB7c_GpxBPI8iKAlXtkJiPZqOfPWjsWQz_iQQCDA9ctMk9fe-1holP1bhw9EpJdmhOyyi-QVA54Bh75yZ3QuLQdBTrqE01yvRwhCOdarxMBhVb5JYum2cq8PRsIoEC2QdYKGgIFaLW-ctZyaz2DRQpTQoDnn4p0xVrwrmYwCBd2gLbl3xKjjUL2bKLfZ-mCUteGZyGEH3bUyiq4htCONMYg4yQekwB-NbnNpIi3uYjnSqNG5chbYxm4t7OpsZ1cAOm86sf84R4VyqIdRqsppxoT2y6HO2ZAzfd87txtjisr1y6BAf-61eyprYOQOv1s0-j49ges_1jWhCfjOBbiF0Avf12z0LFcRSXk2W6_zfHiRPc7SMNnk2u9ovZNzutprNKYexc92POX4oPW7SumjbO_kUz8K2bptAknobl68W0E6KpM2Brlu1GJp-buRtREOFil2AoObsIb_2SeDENQJwqW3mb9WFzvC9dGduuwpCrO8AiXYIMuywBa76nroGMRMl0r5fYzfZq-AfxbBJohwIDVsHhV1lY");
+        $tokenService = $this->jwtDecodeService->tokenDecode("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE1OTQxOTEzMzQsImV4cCI6MTU5NDE5NDkzNCwicm9sZXMiOlsiUk9MRV9VU0VSIl0sInVzZXJuYW1lIjoicXVlbnRpbi5iYXJyYXVkQGdtYWlsLmNvbSJ9.A58j6vBD19247LjpI0z1JKHRTLeATdC45hDy-GN47zT0y0Sf-0X9HTwNLBhKT4XW-IyNalxt9L_8h6w6qYNhIgvQ2o5O0LXbuC9EUhAxyAPkcDwKEJe9uLx4zkcGkphPYO4JXmVPO_KiB6f23ALC-Z_umluX15FC_dnx4LhM77IAQ1Zq2egNISQYnicdsvKM3_IFtZISxgKDSkUNx3ITexYlysosUwqEsBOFn6O8iPExWFcuFbToUwIdKY_EdwINJUNOO4zqX_jHWVA6FX-3Zb0lWhIQIgdJIbs1UfhNtfbcsfA1hkjThvg679LIHiFzFPaPFttmLi0Jf5FVLkWCbObwIhxQq2eWw7ITpnjAPjN-fI6lfgwjHD-oMD3OQLeg_1lHurr8Vpc7SJAcXIl492YH5r-GkqMFJf8Al8ZmkwGLfHP1ThsIQsS_TwL12G2eWC2V1N_s0kpr7QCcMDqnm0i1i-WRLBPkQb45bjqTeW32TtcPspRMQ0RdVqSxzfHYjwZc0UeE_f1zMAxLOv1mR2bO7oUJ7Jt_u69Vc__jEbXPra8Vr02YCibbaePdi2qTYXmsp8bVr2T73XkZ5Ki3-gUdy8iix6TeSU-Om8XmwgLtWHQ4LGEesnyOdgI7vnC78a8DW4lZN04XFQwWJ1HDF2V9i23KhMlQlvO-QYHhOkQ");
         $user = $this->userRepository->findByEmail($tokenService->username);
 
         $userMoodDateAll = $user->getUserMoodDates()->getValues();
@@ -51,10 +51,12 @@ class ActivityController extends AbstractController
         $randomIdea = array_rand($ideasAll, 5);
         $tableIdea = [];
         foreach ($randomIdea as $key => $value) {
-            $tableIdea[]['name'] = $ideasAll[$value]->getName();
-            $tableIdea[]['picture'] = $ideasAll[$value]->getPicture();
+            $tableIdea[] = [
+                    "name" => $ideasAll[$value]->getName(),
+                    "picture" => $ideasAll[$value]->getPicture()
+            ];
         }
 
-        return new JsonResponse(['suggestion' => true, 'ideas' => [$tableIdea]], Response::HTTP_FORBIDDEN);
+        return new JsonResponse(['suggestion' => true, 'ideas' => $tableIdea], Response::HTTP_OK);
     }
 }
