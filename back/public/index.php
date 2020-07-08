626c8d1f5eb77d90a74513bb098d992f09d1db98
Theraen <barraud.quentin@gmail.com>
2020-07-08 15:35:25 +0200

    :wrench: Remove Header With Credentials

--- back/public/index.php
+++ back/public/index.php
@@ -24,7 +24,6 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
 }
 
 header('Access-Control-Allow-Origin: http://localhost:8080');
-header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
