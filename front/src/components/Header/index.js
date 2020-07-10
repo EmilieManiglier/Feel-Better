12f20ddb671869d7f666442a7cf1b2c68389a173
Emilie-Excalibur <emiliemaniglier@gmail.com>
2020-07-10 08:44:52 +0200

    :lipstick: Add toggle switch

--- front/src/components/Header/index.js
+++ front/src/components/Header/index.js
@@ -39,6 +39,22 @@ const Header = ({ isLogged, logout }) => {
           </Link>
         </h1>
         <ul className={addClass ? 'main-nav active' : 'main-nav'}>
+          <div className="toggleWrapper">
+            <input type="checkbox" className="dn" id="dn" />
+            <label htmlFor="dn" className="toggle">
+              <span className="toggle__handler">
+                <span className="crater crater--1" />
+                <span className="crater crater--2" />
+                <span className="crater crater--3" />
+              </span>
+              <span className="star star--1" />
+              <span className="star star--2" />
+              <span className="star star--3" />
+              <span className="star star--4" />
+              <span className="star star--5" />
+              <span className="star star--6" />
+            </label>
+          </div>
           <li>
             <NavLink
               exact
@@ -89,7 +105,7 @@ const Header = ({ isLogged, logout }) => {
             >
               Activités
             </NavLink>
-          </li>
+          </li> 
           {isLogged && (
             <li>
               <NavLink
