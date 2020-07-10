d799f0ed606f4ade187cb561c80a3cbe0f3faabd
Theraen <barraud.quentin@gmail.com>
2020-07-10 15:27:09 +0200

    :lock: Add Authenticvator for administration

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Service\JwtDecodeService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -25,5 +26,31 @@ class SecurityController extends AbstractController
     {
         $jsonData = json_decode($request->getContent());
         return $this->jwtDecodeService->tokenVerifyUser($jsonData->token);
+    }
+
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        //if ($this->getUser()) {
+        //    return $this->redirectToRoute('easyadmin');
+        //}
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     */
+    public function logout()
+    {
+        // controller can be blank: it will never be executed!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
--- back/src/Entity/UserMoodDate.php
+++ back/src/Entity/UserMoodDate.php
@@ -45,11 +45,6 @@ class UserMoodDate
         $this->moods = new ArrayCollection();
     }
 
-    public function __toString()
-    {
-        return $this->getMoodDate();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
